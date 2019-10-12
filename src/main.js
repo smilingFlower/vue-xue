e1b3d90312cd2d7a9a4d78a12dc85a5c13f91552
wxh <yyy_wintercherry@163.com>
2019-10-12 14:31:53 +0800

    使用VueMeta进行head meta设置管理

--- src/main.js
+++ src/main.js
@@ -9,6 +9,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueMeta from 'vue-meta'
+
+Vue.use(VueMeta)
+
+Vue.mixin({
+  metaInfo: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes' }
+    ]
+  }
+})
 
 Vue.config.productionTip = false
 
